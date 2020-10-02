be6c999d75020caa8bdf50bd638aaa22dc444580
Shahin-Mia <shahin.cmt2@gmail.com>
2020-10-02 07:16:15 +0600

    intigreted node-mongo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -29,19 +29,13 @@ const Login = () => {
 
         firebase.auth().signInWithPopup(provider).then(function (result) {
             // This gives you a Google Access Token. You can use it to access the Google API.
-            const token = result.credential.accessToken;
             const { displayName, email } = result.user;
             const signInUser = { displayName, email };
             setLoggedInUser(signInUser);
             storeAuthToken();
         }).catch(function (error) {
             // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            const credential = error.credential;
+            console.log(error);
             // ...
         });
     }
